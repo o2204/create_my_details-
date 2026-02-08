6cf1616c7fe0b2cc26ad03c482b877e951a6cb2d
Omar Atef <o.atef35251@gmail.com>
2026-02-08 16:45:21 +0200

    update code and fix errors

--- service/create_my_details_service.py
+++ service/create_my_details_service.py
@@ -1,15 +1,27 @@
 from schemas.create_my_details_schemas import CreateMyRequestSchema, CreateMyResponseSchema
 from repo.create_my_details_repo import CreateMyDetailsRepo
-
+from exceptions.create_my_details_exception import (
+    NameNotFound,
+    AgeNotFound,
+    AddressNotFound,
+)
 
 class CreateMyDetailsService:
     def __init__(self, repo:CreateMyDetailsRepo):
         self.repo = repo 
-
+    
     async def create_my_details(
             self,
             data: CreateMyRequestSchema,
     ) -> CreateMyResponseSchema:
+        
+        if not data.name or not data.name.strip():
+            raise NameNotFound(data.name, exception_type="This's in service layer") 
+        if data.age is None or data.age <= 0:
+            raise AgeNotFound(data.age)
+
+        if not data.address or not data.address.strip():
+            raise AddressNotFound(data.address)
         
         obj = await self.repo.create(data)
 
