86dbbfbf344dac455d029c0763bf751f39970395
Omar Atef <o.atef35251@gmail.com>
2026-02-20 16:17:59 +0200

    feat: upload file and get response

--- core/config.py
+++ core/config.py
@@ -8,10 +8,10 @@ class Settings(BaseSettings):
     SUPABASE_KEY: str 
 
     COHERE_API_KEY: str 
+    OPENAI_API_KEY: str 
 
     class Config:
         env_file = ".env"
 
 def get_settings():
     return Settings()
-
